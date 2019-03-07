d2741f1be01814a066979656f7f88210a9ce77bd
ペリーも恐れるペリカン <kazuyaman1413@gmail.com>
2019-03-07 21:26:34 +0900

    finall comit

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -11,13 +11,13 @@ class FavoritesController < ApplicationController
     if favorite.save
       redirect_to topics_path, success: 'あなたのParadiseに認定したよ＾＾'
     else
-      redirect_to topics_path, danger: 'あなたのParadiseには及ばないよ：（'
+      redirect_to topics_path
     end
   end
 
   def destroy
     favorite = Favorite.find_by(user_id: current_user.id ,topic_id: params[:topic_id])
     favorite.destroy
-    redirect_to topics_path, warnig: 'false'
+    redirect_to topics_path, danger: 'あなたのParadiseには及ばないよ：（'
   end
 end
